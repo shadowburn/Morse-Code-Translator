21222190e7a6e883de8d7aef8038e596fd19a5ec
Jizi Zhang <zjzottawa@gmail.com>
2017-04-17 02:31:34 -0400

    Dictionary created

--- MorseCodeTranslator/MorseCodeTranslator.cs
+++ MorseCodeTranslator/MorseCodeTranslator.cs
@@ -1,12 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MorseCodeTranslator
 {
-    class MorseCodeTranslator
+    static class MorseCodeTranslator
     {
+        private static Dictionary<char, string> _textToMorse = new Dictionary<char, string>
+        {
+            {' ', "/"},
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."},
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."},
+            {'0', "-----"},
+            {',', "--..--"},
+            {'.', ".-.-.-"},
+            {'?', "..--.."},
+            {';', "-.-.-."},
+            {':', "---..."},
+            {'/', "-..-."},
+            {'-', "-....-"},
+            {'\'', ".----."},
+            {'"', ".-..-."},
+            {'(', "-.--."},
+            {')', "-.--.-"},
+            {'=', "-...-"},
+            {'+', ".-.-."},
+            {'@', ".--.-."},
+            {'!', "-.-.--"},
+            {'Á', ".--.-"},
+            {'É', "..-.."},
+            {'Ö', "---."},
+            {'Ä', ".-.-"},
+            {'Ñ', "--.--"},
+            {'Ü', "..--"}
+        };
+
+        public static string ToMorse(string input)
+        {
+            List<string> output = new List<string>(input.Length);
+
+            foreach(char character in input.ToUpper())
+            {
+                try 
+                {
+                    string morseCode = _textToMorse[character];
+                    output.Add(morseCode);
+                }
+                catch(KeyNotFoundException)
+                {
+                    output.Add("!");
+                }
+                
+            }
+
+            return string.Join(" ", output);
+        }
     }
 }
--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -8,8 +8,20 @@ namespace MorseCodeTranslator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write(": ");
+                string input = Console.ReadLine();
+                
+                if(string.IsNullOrWhiteSpace(input))
+                { break; }
+
+                string output = MorseCodeTranslator.ToMorse(input);
+
+                Console.WriteLine(output);
+            }
         }
     }
 }
